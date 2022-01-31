7d5b09e48ec3de00236870d6a849f60f1bc7c316
unknown <goncalodias.multimedia@gmail.com>
2022-01-31 22:04:45 +0000

    Create new project with changed package versions

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import 'tailwindcss/tailwind.css'
 
 import Layout from "../components/layouts/Layout";
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,23 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: "class", // or 'media' or 'class'
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
   theme: {
     extend: {
       fontFamily: {
-        sans: ["Montserrat", ...defaultTheme.fontFamily.sans],
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
       },
       backgroundImage: () => ({
         hero: "url('/img/restaurante-14.jpg')",
       }),
       colors: {
-        "trovador-primary": "#D9D1B8",
-        "trovador-background": "#FBF8F4",
+        'trovador-primary': '#D9D1B8',
+        'trovador-background': '#FBF8F4',
       },
     },
   },
   plugins: [],
-};
+}
